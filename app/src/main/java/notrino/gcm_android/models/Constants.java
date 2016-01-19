151c12ffa4c18e7bce30febac74610ccba1d7c0f
David Zhang <david8zhang@berkeley.edu>
2016-01-19 15:12:28 -0800

    added Android project

---
+++ app/src/main/java/notrino/gcm_android/models/Constants.java
@@ -0,0 +1,12 @@
+package notrino.gcm_android.models;
+
+/**
+ * Created by david_000 on 1/19/2016.
+ */
+public class Constants {
+    public final static String SENDER_ID = "578010136131";
+    public final static String CREATE_USER_API = "https://fast-taiga-6198.herokuapp.com/api/v1/gcm/users/register";
+    public final static String CREATE_GCM_API = "https://fast-taiga-6198.herokuapp.com/api/v1/gcm/upload";
+    public final static String INDEX_USER_API = "https://fast-taiga-6198.herokuapp.com/api/v1/gcm/users";
+    public final static String INDEX_GCM_API = "https://fast-taiga-6198.herokuapp.com/api/v1/gcm/index";
+}
---
+++ app/src/main/java/notrino/gcm_android/utils/ApiManager.java
@@ -0,0 +1,54 @@
+package notrino.gcm_android.utils;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import notrino.gcm_android.controllers.AppController;
+import notrino.gcm_android.models.Constants;
+import notrino.gcm_android.models.DataRequest;
+
+/**
+ * Created by david_000 on 1/19/2016.
+ */
+public class ApiManager {
+
+    /** The current application context. (Used for AWS) */
+    private Context context;
+
+    public ApiManager(Context context) {
+        this.context = context;
+    }
+
+    /** Create a new user. (Happens when onboarding). */
+    public void createUser(HashMap<String, String> params) {
+        InstanceID instanceID = InstanceID.getInstance(context);
+        try {
+            String reg_token = instanceID.getToken(Constants.SENDER_ID, GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            params.put("reg_token", reg_token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DataRequest request = new DataRequest(Request.Method.POST, null, params, Constants.CREATE_USER_API, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                System.out.println(jsonObject);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                System.out.println(volleyError);
+            }
+        });
+        AppController.getInstance().addToRequestQueue(request);
+    }
+}
